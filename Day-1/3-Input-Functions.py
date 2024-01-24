127e916de62385cd00a29eb73d6cfe84e126edc9
Abdul-Ghaniy <abdul.code254@gmail.com>
2024-01-24 09:53:25 +0300

    Band Name Generator

---
+++ Day-1/3-Input-Functions.py
@@ -0,0 +1,11 @@
+#  Input Function
+
+# input("What is your name? ")
+# print("Hello" + input("What is your name?") )
+
+# Instructions
+# Write a program that prints the number of characters in a user's name. 
+# You might need to Google for a function that calculates the length of a string.
+
+
+print(len(input("What is your name? ")))

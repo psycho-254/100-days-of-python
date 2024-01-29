0b583da8a889f08ba5d0a1d898884e90789c8560
Abdul-Ghaniy <abdul.code254@gmail.com>
2024-01-29 21:42:00 +0300

    Python Loops

--- Day-5/Final-Project_ Create a Password Generator.py
+++ Day-5/Final-Project_ Create a Password Generator.py
@@ -1,12 +1,12 @@
 # Password Generator
 
-Instructions
-The program will ask:
-How many letters would you like in your password?
-How many symbols would you like?
-How many numbers would you like?
-The objective is to take the inputs from the user to these questions and then generate a random password. 
-Use your knowledge about Python lists and loops to complete the challenge.
+# Instructions
+# The program will ask:
+# How many letters would you like in your password?
+# How many symbols would you like?
+# How many numbers would you like?
+# The objective is to take the inputs from the user to these questions and then generate a random password. 
+# Use your knowledge about Python lists and loops to complete the challenge.
 
 import random
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']

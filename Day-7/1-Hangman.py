51ee422626a8c03a8b569f8a7877591d57bd851e
Abdul-Ghaniy <abdul.code254@gmail.com>
2024-02-09 20:32:33 +0300

    Hangman

--- Day-7/1-Hangman.py
+++ Day-7/1-Hangman.py
@@ -1,25 +1,12 @@
 #Step 1 
 
-word_list = ["aardvark", "baboon", "camel"]
-
 #TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
-
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-
 #TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
 
-import random
 
-chosen_word = random.choice(word_list)
-print(chosen_word)
-guess = input("Guess a letter: ").lower()
-
-# for n in range(0, len(chosen_word)):
-#     if guess == chosen_word[n]:
-#         print(guess)
 
 #Step 2
-
 
 #TODO-1: - Create an empty List called display.
 #For each letter in the chosen_word, add a "_" to 'display'.
@@ -32,19 +19,84 @@ guess = input("Guess a letter: ").lower()
 #TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
 #Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
 
-display = []
-for n in range(0, len(chosen_word)):
-    display += '_'
-    if guess == chosen_word[n]:
-        display[n] = chosen_word[n]
-print(display)
-
-
 #Step 3
 
 #TODO-1: - Use a while loop to let the user guess again. 
 # The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' has no more blanks ("_"). 
 # Then you can tell the user they've won.
+
+word_list = ["aardvark", "baboon", "camel"]
+import random
+chosen_word = random.choice(word_list)
+print(chosen_word)
+guess = input("Guess a letter: ").lower()
+lives = 6
+display=[]
+
+# while '_' in display:
+for n in range(len(chosen_word)):
+    display += '_'
+    if guess == chosen_word[n]:
+        display[n] = guess
+print(display)
+    # if '_' not in display:
+        # print('you won')    
+
+
+
+#step 4
+
+#TODO-1: - Create a variable called 'lives' to keep track of the number of lives left. 
+#Set 'lives' to equal 6.
+
+#TODO-2: - If guess is not a letter in the chosen_word,
+    #Then reduce 'lives' by 1. 
+    #If lives goes down to 0 then the game should stop and it should print "You lose."
+
+#TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
+
+
+
+# for n in range(0, len(chosen_word)):
+
+# end_game = False
+# while end_game is not True:
+#     lives = 6
+#     guess = input("Guess another letter: ").lower()
+#     for n in range(0, len(chosen_word)):
+#         guess = input("Guess a letter: ").lower()
+#         display = []
+#             display += '_'
+#             if guess == chosen_word[n]:
+#                 display[n] = chosen_word[n]
+#         print(display)
+#         if guess != chosen_word[n]:
+#             lives -= 1
+#             if lives == 0:
+#                 end_game = True
+#                 print("you lose")
+#         elif guess == chosen_word[n]:
+#             display[n] = chosen_word[n]
+#     print(display)
+
+#     if '_' not in display:
+#         print("you won")
+
+
+
+
+
+
+
+
+
+
+
+
+
+# for n in range(0, len(chosen_word)):
+#     if guess == chosen_word[n]:
+#         print(guess)
 
 # while len('_') in display:
 #     for n in range(0, len(chosen_word)):
@@ -63,29 +115,3 @@ print(display)
 
 #     if '_' not in display:
 #         print("you won")
-
-#step 4
-
-#TODO-1: - Create a variable called 'lives' to keep track of the number of lives left. 
-#Set 'lives' to equal 6.
-
-#TODO-2: - If guess is not a letter in the chosen_word,
-    #Then reduce 'lives' by 1. 
-    #If lives goes down to 0 then the game should stop and it should print "You lose."
-
-#TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
-
-lives = 6
-while "_" in display:
-    guess = input("Guess another letter: ").lower()
-    for n in range(0, len(chosen_word)):
-        if lives == 0:
-            print("you lose")
-        if guess != chosen_word[n]:
-            lives -= 1
-        elif guess == chosen_word[n]:
-            display[n] = chosen_word[n]
-    print(display)
-
-    if '_' not in display:
-        print("you won")

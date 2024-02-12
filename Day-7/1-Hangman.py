019d38d4a66b96eca026dc48d6037df1ce492a3d
Abdul-Ghaniy <abdul.code254@gmail.com>
2024-02-12 11:37:08 +0300

    Hangman

--- Day-7/1-Hangman.py
+++ Day-7/1-Hangman.py
@@ -45,30 +45,49 @@ print(chosen_word)
 guess = input("Guess a letter: ").lower()
 lives = 6
 display=[]
+guessed_leters = []
 
 for n in range(len(chosen_word)):
         display += '_'
 end_game = False
 while end_game == False:
+    if guess in guessed_leters:
+        print(f'you,ve already guessed this letter: {guess}. Try a different letter')
     for n in range(len(chosen_word)):
         if guess == chosen_word[n]:
             display[n] = guess
+            guessed_leters.append(guess)
     print(display)
+    
     if guess not in chosen_word:
         lives -= 1
         print(stages[lives])
+        print(f'sorry {guess} is not in the word. you lose a life')
     if lives == 0:
         end_game = True
         print('you lost')
     elif '_' not in display:
         end_game = True
-        print('you won')
+        word = ''.join(display)
+        print(f'You won! The correct answer is {word}')
+        
     else:
         guess = input("Guess another letter: ").lower()   
 
 
+#Step 5
 
 
+#TODO-1: - Update the word list to use the 'word_list' from hangman_words.py
+#TODO-2: - Import the stages from hangman_art.py and make this error go away.
+#TODO-3: - Import the logo from hangman_art.py and print it at the start of the game.
+#TODO-4: - If the user has entered a letter they've already guessed, print the letter and let them know.
+#TODO-5: - If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
+    #Join all the elements in the list and turn it into a String.
+
+   
+
+   
 
 
 

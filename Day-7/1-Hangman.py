96fa018ef48810ef98448bdd7aa0b6a5822a830c
Abdul-Ghaniy <abdul.code254@gmail.com>
2024-02-14 20:42:25 +0300

    Hangman

--- Day-7/1-Hangman.py
+++ Day-7/1-Hangman.py
@@ -1,12 +1,9 @@
-#Step 1 
-
+##Step 1 
 #TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 #TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
 
-
-#Step 2
-
+##Step 2
 #TODO-1: - Create an empty List called display.
 #For each letter in the chosen_word, add a "_" to 'display'.
 #So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
@@ -18,15 +15,12 @@
 #TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
 #Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
 
-#Step 3
-
+##Step 3
 #TODO-1: - Use a while loop to let the user guess again. 
 # The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' has no more blanks ("_"). 
 # Then you can tell the user they've won.
 
-
-#step 4
-
+##step 4
 #TODO-1: - Create a variable called 'lives' to keep track of the number of lives left. 
 #Set 'lives' to equal 6.
 
@@ -36,6 +30,15 @@
 
 #TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
 
+##Step 5
+#TODO-1: - Update the word list to use the 'word_list' from hangman_words.py
+#TODO-2: - Import the stages from hangman_art.py and make this error go away.
+#TODO-3: - Import the logo from hangman_art.py and print it at the start of the game.
+#TODO-4: - If the user has entered a letter they've already guessed, print the letter and let them know.
+#TODO-5: - If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
+    #Join all the elements in the list and turn it into a String.
+
+   
 import random
 from Hangman_art import stages, logo
 from Hangman_words import word_list
@@ -74,94 +77,4 @@ while end_game == False:
     else:
         guess = input("Guess another letter: ").lower()   
 
-
-#Step 5
-
-
-#TODO-1: - Update the word list to use the 'word_list' from hangman_words.py
-#TODO-2: - Import the stages from hangman_art.py and make this error go away.
-#TODO-3: - Import the logo from hangman_art.py and print it at the start of the game.
-#TODO-4: - If the user has entered a letter they've already guessed, print the letter and let them know.
-#TODO-5: - If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
-    #Join all the elements in the list and turn it into a String.
-
    
-
-   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# for n in range(0, len(chosen_word)):
-
-# end_game = False
-# while end_game is not True:
-#     lives = 6
-#     guess = input("Guess another letter: ").lower()
-#     for n in range(0, len(chosen_word)):
-#         guess = input("Guess a letter: ").lower()
-#         display = []
-#             display += '_'
-#             if guess == chosen_word[n]:
-#                 display[n] = chosen_word[n]
-#         print(display)
-#         if guess != chosen_word[n]:
-#             lives -= 1
-#             if lives == 0:
-#                 end_game = True
-#                 print("you lose")
-#         elif guess == chosen_word[n]:
-#             display[n] = chosen_word[n]
-#     print(display)
-
-#     if '_' not in display:
-#         print("you won")
-
-
-
-
-
-
-
-
-
-
-
-
-
-# for n in range(0, len(chosen_word)):
-#     if guess == chosen_word[n]:
-#         print(guess)
-
-# while len('_') in display:
-#     for n in range(0, len(chosen_word)):
-#         display += '_'
-#         if guess == chosen_word[n]:
-#             display[n] = chosen_word[n]
-#             display -= '_'
-#     print(display)
-
-# while "_" in display:
-#     guess = input("Guess another letter: ").lower()
-#     for n in range(0, len(chosen_word)):
-#         if guess == chosen_word[n]:
-#             display[n] = chosen_word[n]
-#     print(display)
-
-#     if '_' not in display:
-#         print("you won")

15d77513fd79ac8599e6ce5ab2cbddf936122537
psycho-254 <abdul.code254@gmail.com>
2024-07-18 14:47:07 +0300

    Escaping-the-Maze

---
+++ Day-6-functions/1-Defining-and-Calling-Python-Functions.py
@@ -0,0 +1,49 @@
+## functions
+
+def my_function():
+  print("hello")
+
+my_function()
+
+#  navigation script for karel the robot using functions
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+turn_left()
+move()
+turn_right()
+move()
+turn_right()
+move()
+turn_right()
+move()
+
+## Hurdles race
+
+# Reeborg has entered a hurdles race. Make him run the course, following the path shown.
+# What you need to know
+# The functions move() and turn_left().
+# More advanced
+# You may have noticed that your solution has some repeated patterns. 
+# If you know how to define functions, define a function named jump() and use it to simplify your program.
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+    
+
+for step in range(6):
+    jump()

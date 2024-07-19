6753c89c0a65aff29675cc383456395825ff99bd
psycho-254 <abdul.code254@gmail.com>
2024-07-19 11:14:22 +0300

    Escaping-the-Maze

---
+++ Day-6-functions/3-while-loops.py
@@ -0,0 +1,33 @@
+## Hurdles race
+
+# Reeborg has entered a hurdle race. Make him run the course, following the path shown.
+# The position and number of hurdles changes each time this world is reloaded.
+# What you need to know
+# The functions move() and turn_left().
+# The conditions front_is_clear() or wall_in_front(), at_goal(), and their negation.
+# How to use a while loop and an if statement.
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    turn_left()
+    while wall_on_right():
+        move()
+    turn_right()
+    move()
+    turn_right()
+    while front_is_clear():
+        move()
+    turn_left()
+    
+
+while not at_goal():
+    if wall_in_front():
+        jump()
+    else:
+        move()
+    
+    
